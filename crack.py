50e9f78caa739ed0cb7ae9caf1ea4239efc67902
Kulsgam <90457704+Kulsgam@users.noreply.github.com>
2023-02-28 00:23:08 +0530

    Update crack.py

--- crack.py
+++ crack.py
@@ -2,4 +2,6 @@ from string import ascii_letters, digits, punctuation
 from itertools import product
 
 for passcode in product(ascii_letters + digits + punctuation, repeat=4):
+    # Product gives a packed object of tuples to all the possible permutations for the "repeat" number specified. '*' is used to unpack the tuples
+    # Eg:- list = ['a', 'b', 'c']; if repeat is 2 ---> ['a', 'b'] and ['b', 'a'] and so on...
     print(*passcode)
