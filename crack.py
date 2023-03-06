7d1850b193082a8e3916a411ee28c24b73f10934
Kulsgam <90457704+Kulsgam@users.noreply.github.com>
2023-03-06 18:02:42 +0530

    Made changes to algorithm
    The string outputted will have all possible number of characters from 1 to passcode_limit.

--- crack.py
+++ crack.py
@@ -1,7 +1,10 @@
 from string import ascii_letters, digits, punctuation
 from itertools import product
 
-for passcode in product(ascii_letters + digits + punctuation, repeat=4):
-    # Product gives a packed object of tuples to all the possible permutations for the "repeat" number specified. '*' is used to unpack the tuples
-    # Eg:- list = ['a', 'b', 'c']; if repeat is 2 ---> ['a', 'b'] and ['b', 'a'] and so on...
-    print(*passcode)
+passcode_limit = 4
+
+for i in range(0, passcode_limit + 1):
+    for passcode in product(ascii_letters + digits + punctuation, repeat=i):
+        # Product gives a packed object of tuples to all the possible permutations for the "repeat" number specified. '*' is used to unpack the tuples
+        # Eg:- list = ['a', 'b', 'c']; if repeat is 2 ---> ['a', 'b'] and ['b', 'a'] and so on...
+        print(*passcode)
